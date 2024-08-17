83e11a5d64d4c4d843b9fc6b41f05c1a498a5d04
Faiyaz Ansari <ansarifaiyaz300@gmail.com>
2024-08-17 15:55:15 +0530

    first commit

---
+++ src/main/java/com/restapi_crud_h2/RestapiCrudH2Application.java
@@ -0,0 +1,13 @@
+package com.restapi_crud_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiCrudH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiCrudH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi_crud_h2/RestapiCrudH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi_crud_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiCrudH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
